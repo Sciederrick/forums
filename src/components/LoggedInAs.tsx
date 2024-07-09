b16418958672cfc8df1c698763606de6b306d684
Sciederrick <derrickmbarani@gmail.com>
2024-07-09 20:04:02 +0300

    feat: include inbox button for DMs

--- src/components/LoggedInAs.tsx
+++ src/components/LoggedInAs.tsx
@@ -1,17 +1,22 @@
 import { useContext } from "react";
 import { AppContext } from "../contexts/AppContext";
+import InboxOutlinedIcon from "@mui/icons-material/InboxOutlined";
 
 const LoggedInAs = () => {
     const ctx = useContext(AppContext);
     return (
-        <div key={ctx?.loggedInAs?._id} className="flex items-center gap-2 p-2 text-sm -ml-4">
+        <div
+            key={ctx?.loggedInAs?._id}
+            className="w-full flex justify-between items-center gap-2 p-2 text-sm"
+        >
             <img
                 src={ctx?.loggedInAs?.avatar}
                 height={32}
                 width={32}
                 className="rounded-full"
+                title={ctx?.loggedInAs?.email}
             />
-            <p>{ctx?.loggedInAs?.email}</p>
+            <button className="flex flex-col items-center"><InboxOutlinedIcon className="text-gray-400"/><p className="text-xs text-gray-400">inbox</p></button>
         </div>
     );
 };

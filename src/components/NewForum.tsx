62bd827500a5d6e37ad79fa7cfc7f162b259d380
Sciederrick <derrickmbarani@gmail.com>
2024-07-23 16:44:23 +0300

    styling: add humans profile results

--- src/components/NewForum.tsx
+++ src/components/NewForum.tsx
@@ -1,10 +1,17 @@
 import ArrowBackIosOutlinedIcon from "@mui/icons-material/ArrowBackIosOutlined";
 import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
-import { useState } from "react";
+import { useContext, useEffect, useState } from "react";
+import debounce from "lodash/debounce";
+import client from "../lib/feathersClient";
+import { User } from "../types";
+import { AppContext } from "../contexts/AppContext";
+import { Divider } from "@mui/material";
 
 const NewGroup = () => {
+    const ctx = useContext(AppContext);
     const [showSearch, setShowSearch] = useState(false);
     const [searchTerm, setSearchTerm] = useState("");
+    const [users, setUsers] = useState<User[]>([]);
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const handleChange = (e: any) => {
         const value = e.target.value;
@@ -13,6 +20,51 @@ const NewGroup = () => {
     const handleToggleSearch = () => {
         setShowSearch((prevState) => !prevState);
     };
+
+    const searchUsers = debounce(async (email) => {
+        try {
+            if (email) {
+                const response = await client.service("users").find({
+                    query: {
+                        // email: { $regex: new RegExp(email, 'i')},
+                        email,
+                        $limit: 10,
+                    },
+                });
+                setUsers(response.data);
+            } else {
+                setUsers([]);
+            }
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        } catch (err: any) {
+            ctx?.onNotif(`failed to search users with err: ${err}`);
+        }
+    }, 300);
+
+    const getAllUsers = async () => {
+        try {
+            const response = await client.service("users").find();
+            setUsers(response.data);
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        } catch (err: any) {
+            ctx?.onNotif(`failed to fetch users with err: ${err}`);
+        }
+    };
+
+    const [isAlreadyExecuted, setIsAlreadyExecuted] = useState(false);
+    useEffect(() => {
+        // Get all users on mounted
+        if (!isAlreadyExecuted) {
+            console.log("🚀 ~ useEffect ~ isAlreadyExecuted:", isAlreadyExecuted)
+            getAllUsers();
+            setIsAlreadyExecuted(true);
+        } else {
+            console.log("🚀 ~ useEffect ~ isAlreadyExecuted:", isAlreadyExecuted)
+			// Search users on search
+			// searchUsers(searchTerm);
+		}
+
+    }, [searchTerm]);
     return (
         <>
             <div className="flex justify-between gap-2 px-3 py-4">
@@ -28,7 +80,7 @@ const NewGroup = () => {
                             autoComplete="true"
                             placeholder="type to search ..."
                             className="w-full h-[32px] focus:outline-none"
-							autoFocus
+                            autoFocus
                         />
                     ) : (
                         <div className="h-[32px] flex flex-col justify-center">
@@ -43,7 +95,23 @@ const NewGroup = () => {
                     </button>
                 )}
             </div>
-            <div>{searchTerm}</div>
+            <Divider />
+            <ul>
+                {users.map((user) => (
+                    <li
+                        key={user._id}
+                        className="flex items-center gap-2 p-4 text-sm cursor-pointer hover:bg-gray-100"
+                    >
+                        <img
+                            src={user.avatar}
+                            width={32}
+                            height={32}
+                            className="rounded-full"
+                        />
+                        <p>{user.email}</p>
+                    </li>
+                ))}
+            </ul>
         </>
     );
 };
